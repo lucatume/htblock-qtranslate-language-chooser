d531fbe723ef42e6bc71a9686ab0ab8b8338f701
Luca Tumedei <luca@theaveragedev.com>
2014-03-03 15:49:39 +0100

    corrected a variable initialization error

--- includes/Block.php
+++ includes/Block.php
@@ -13,26 +13,29 @@ class Block extends \HeadwayBlockAPI
     {
         if (function_exists('qtrans_generateLanguageSelectCode')) {
             $displayMode = \HeadwayBlocksData::get_block_setting($block, 'display-mode', 'dropdown');
-            $out .= qtrans_generateLanguageSelectCode($displayMode);
+            $out = qtrans_generateLanguageSelectCode($displayMode);
             echo $out;
         }
     }
-    // public function dynamic_css($block_id, $block, $original_block = null) {
-    //     $selector = '#block-' . $block_id;
-    //     if ( is_array($original_block) ) {
-    //         $block_id = $original_block['id'];
-    //         $block = $original_block;
-    //         $selector .= '.block-original-' . $block_id;
-    //     }
-    //         $layoutMode = \HeadwayBlocksData::get_block_setting($block, 'layout-mode', 'horizontal');
-    //         $out = '';
-    //         $out .= $selector . ' .qtblock-language-selection ul'
-    //         return $out;
-    //     return '
-    //     ' . $selector . ' .nav-horizontal ul.menu > li > a,
-    //     ' . $selector . ' .nav-search-active .nav-search {
-    //         height: ' . $block_height . 'px;
-    //         line-height: ' . $block_height . 'px;
-    //     }';
-    // } 
+    public function dynamic_css($block_id, $block, $original_block = null)
+    {
+        $selector = '#block-' . $block_id;
+        if (is_array($original_block)) {
+            $block_id = $original_block['id'];
+            $block = $original_block;
+            $selector .= '.block-original-' . $block_id;
+        }
+        $layoutMode = \HeadwayBlocksData::get_block_setting($block, 'layout-mode', 'horizontal');
+        $out = '';
+        if ($layoutMode == 'horizontal') {
+            $out .= $selector . ' ul.qtrans_language_chooser > li {display:inline-block;}';
+        }
+        return $out;
+        // return '
+        // ' . $selector . ' .nav-horizontal ul.menu > li > a,
+        // ' . $selector . ' .nav-search-active .nav-search {
+        //     height: ' . $block_height . 'px;
+        //     line-height: ' . $block_height . 'px;
+        // }';
+    } 
 }
