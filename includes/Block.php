074ed78e9f690e7c2751d166f8d35a4190dac30a
Luca Tumedei <luca@theaveragedev.com>
2014-03-03 15:41:46 +0100

    removed unused markup

--- includes/Block.php
+++ includes/Block.php
@@ -13,11 +13,26 @@ class Block extends \HeadwayBlockAPI
     {
         if (function_exists('qtrans_generateLanguageSelectCode')) {
             $displayMode = \HeadwayBlocksData::get_block_setting($block, 'display-mode', 'dropdown');
-            $out = '';
-            $out .= '<div class="qtblock-language-selection">';
             $out .= qtrans_generateLanguageSelectCode($displayMode);
-            $out .= '</div>';
             echo $out;
         }
     }
+    // public function dynamic_css($block_id, $block, $original_block = null) {
+    //     $selector = '#block-' . $block_id;
+    //     if ( is_array($original_block) ) {
+    //         $block_id = $original_block['id'];
+    //         $block = $original_block;
+    //         $selector .= '.block-original-' . $block_id;
+    //     }
+    //         $layoutMode = \HeadwayBlocksData::get_block_setting($block, 'layout-mode', 'horizontal');
+    //         $out = '';
+    //         $out .= $selector . ' .qtblock-language-selection ul'
+    //         return $out;
+    //     return '
+    //     ' . $selector . ' .nav-horizontal ul.menu > li > a,
+    //     ' . $selector . ' .nav-search-active .nav-search {
+    //         height: ' . $block_height . 'px;
+    //         line-height: ' . $block_height . 'px;
+    //     }';
+    // } 
 }
