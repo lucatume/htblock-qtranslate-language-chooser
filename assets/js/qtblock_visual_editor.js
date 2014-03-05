417b9aa7b9ef9a608a199f113ce213dbf6085e44
Luca Tumedei <luca@theaveragedev.com>
2014-03-05 13:33:40 +0100

    modified the js code to keep it dry

--- assets/js/qtblock_visual_editor.js
+++ assets/js/qtblock_visual_editor.js
@@ -3,33 +3,40 @@
  * Copyright (c) 2014; * Licensed GPLv2+ */
 jQuery(document).ready(function($) {
     'use strict';
-    // the select that will trigger the showing or hiding of settings
+
+    function maybeHideOptions(current) {
+        var selected = current.find('option:selected').text();
+        if (selected === '') {
+            // if no option selected then do nothing
+            return;
+        }
+        // get all actors and targets
+        var parent = $('#sub-tab-settings-content').has(current);
+        var layoutModeSelector = parent.find('#input-layout-mode');
+        var horizontalSpacingSelector = parent.find('#input-horizontal-spacing');
+        var verticalSpacingSelector = parent.find('#input-vertical-spacing');
+        // if the selected option is dropdown then
+        // hide non relevant options
+        if (selected === 'Dropdown') {
+            // hide all the other selectors
+            layoutModeSelector.hide();
+            horizontalSpacingSelector.hide();
+            verticalSpacingSelector.hide();
+        } else {
+            // show the other selectors
+            layoutModeSelector.show();
+            horizontalSpacingSelector.show();
+            verticalSpacingSelector.show();
+        }
+    }
+    // run the script on each select
     jQuery('.panel-block-type-qtblock #input-display-mode select').each(
-        function(index, Element) {
+        function() {
+            maybeHideOptions($(this));
+            // when selected option change make a check and
+            // and hide or show options
             $(this).change(function() {
-                var current = $(this);
-                var selected = current.find('option:selected').text();
-                if (selected === '') {
-                    // if no option selected then do nothing
-                    return;
-                }
-                var parent = $('#sub-tab-settings-content').has(current);
-                // if the selected option is dropdown then
-                // hide non relevant options
-                var layoutModeSelector = parent.find('#input-layout-mode');
-                var horizontalSpacingSelector = parent.find('#input-horizontal-spacing');
-                var verticalSpacingSelector = parent.find('#input-vertical-spacing');
-                if (selected === 'Dropdown') {
-                    // hide all the other selectors
-                    layoutModeSelector.hide();
-                    horizontalSpacingSelector.hide();
-                    verticalSpacingSelector.hide();
-                } else {
-                    // show the other selectors
-                    layoutModeSelector.show();
-                    horizontalSpacingSelector.show();
-                    verticalSpacingSelector.show();
-                }
+                maybeHideOptions($(this));
             });
         });
 });
