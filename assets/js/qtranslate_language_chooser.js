3d237e4129fec005b6f8ca3acb5735428e7efee6
Luca Tumedei <luca@theaveragedev.com>
2014-03-05 13:35:36 +0100

    recompiled the files

---
+++ assets/js/qtranslate_language_chooser.js
@@ -0,0 +1,8 @@
+/*! qTranslate language chooser - v0.1.0
+ * http://theAverageDev.com
+ * Copyright (c) 2014; * Licensed GPLv2+ */
+( function( window, undefined ) {
+	'use strict';
+
+
+} )( this );
