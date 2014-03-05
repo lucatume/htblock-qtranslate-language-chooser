a66048958919cebd4998520073f9c1aad3a99f9f
Luca Tumedei <luca@theaveragedev.com>
2014-03-05 11:21:21 +0100

    first useful working version of the script

--- assets/js/qtblock_visual_editor.js
+++ assets/js/qtblock_visual_editor.js
@@ -2,10 +2,34 @@
  * http://theAverageDev.com
  * Copyright (c) 2014; * Licensed GPLv2+ */
 jQuery(document).ready(function($) {
-   'use strict';
+    'use strict';
     // the select that will trigger the showing or hiding of settings
-    var select = jQuery('.panel-block-type-qtblock #input-display-mode select');
-    select.change(function(select){
-        select.css('background-color', 'yellow');
-    });
+    jQuery('.panel-block-type-qtblock #input-display-mode select').each(
+        function(index, Element) {
+            $(this).change(function() {
+                var current = $(this);
+                var selected = current.find('option:selected').text();
+                if (selected === '') {
+                    // if no option selected then do nothing
+                    return;
+                }
+                var parent = $('#sub-tab-settings-content').has(current);
+                // if the selected option is dropdown then
+                // hide non relevant options
+                var layoutModeSelector = parent.find('#input-layout-mode');
+                var horizontalSpacingSelector = parent.find('#input-horizontal-spacing');
+                var verticalSpacingSelector = parent.find('#input-vertical-spacing');
+                if (selected === 'Dropdown') {
+                    // hide all the other selectors
+                    layoutModeSelector.hide();
+                    horizontalSpacingSelector.hide();
+                    verticalSpacingSelector.hide();
+                } else {
+                    // show the other selectors
+                    layoutModeSelector.show();
+                    horizontalSpacingSelector.show();
+                    verticalSpacingSelector.show();
+                }
+            });
+        });
 });
